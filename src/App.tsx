c762855feb07748d81959161f0acc7c7e270eb55
Animechill30 <82389963+AmerUkmata@users.noreply.github.com>
2022-08-02 19:09:44 +0200

    a

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,81 @@
-import { useState } from 'react'
-import './App.css'
+import { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Post from "./components/Post";
+import { CreatePost } from "./components/CreatePost";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [postImages, setPostImages] = useState([]);
+  const [postComments, setPostComments] = useState([]);
+
+  function like(post: any) {
+    let copyPostImages = [...postImages];
+    let match: any = copyPostImages.find((copyPost) => post.id === copyPost.id);
+    match.likes++;
+    setPostImages(copyPostImages);
+    fetch(`http://localhost:3006/images/${match.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(match),
+    });
+  }
+  function addComment(content: string, imageId: number) {
+    fetch("http://localhost:3006/comments", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ content, imageId }),
+    })
+      .then((resp) => resp.json())
+      .then((comment) => {
+        setPostComments([...postComments, comment]);
+      });
+  }
+
+  function createPost(title: string, image: string) {
+    fetch("http://localhost:3006/images", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ title, image, likes: 0 }),
+    })
+      .then((resp) => resp.json())
+      .then((newPost) => {
+        setPostImages([...postImages, newPost]);
+      });
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:3006/comments")
+      .then((resp) => resp.json())
+      .then((comments) => setPostComments(comments));
+
+    fetch("http://localhost:3006/images")
+      .then((resp) => resp.json())
+      .then((images) => setPostImages(images));
+  }, []);
 
   return (
-     <div className="App">
-       
-    <img className="logo" src="assets/hoxtagram-logo.png" />
-
-    
-    <section className="image-container">
-      
-
-      <article className="image-card">
-        <h2 className="title">Title of image goes here</h2>
-        <img src="./assets/image-placeholder.jpg" className="image" />
-        <div className="likes-section">
-          <span className="likes">0 likes</span>
-          <button className="like-button">♥</button>
-        </div>
-        <ul className="comments">
-          <li>Get rid of these comments</li>
-          <li>And replace them with the real ones</li>
-          <li>From the server</li>
-        </ul>
-      </article>
-    </section>
-     </div>
-  )
+    <>
+      <img src="src/assets/hoxtagram-logo.png" alt="" />
+      <CreatePost createPost={createPost} />
+      <div className="App image-container">
+        {postImages.map((images) => (
+          <Post
+            images={images}
+            comments={postComments}
+            key={images.id}
+            like={like}
+            addComment={addComment}
+          />
+        ))}
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
